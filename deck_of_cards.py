1d4dce447e198c0245e0906efd5abd24fb410252
williamjohngardner <bill@williamjohngardner.com>
2016-05-25 15:14:54 -0400

    First Commit

--- deck_of_cards.py
+++ deck_of_cards.py
@@ -1,13 +1,16 @@
 class Deck_of_Cards:
 
     def __init__(self):
-        num_cards = 52
-        num_suits = 13
-        num_value = 4
+        pass
+        # num_cards = 52
+        # num_suits = 13
+        # num_value = 4
 
     def deal_cards(self):
+        pass
         # random number in range to determine dealt cards
 
     def shuffle_cards(self):
+        pass
         # reset random number counter?
 
---
+++ hand.py
@@ -0,0 +1,13 @@
+class Hand:
+
+    def __init__(self, dealer):
+        self.dealer = False
+
+    def show_hide(self):
+        pass
+
+    def hit(self):
+        pass
+
+    def stand(self):
+        pass
