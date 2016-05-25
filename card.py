df198459f492443c6864c7eea42a40fc5b8d77c5
williamjohngardner <bill@williamjohngardner.com>
2016-05-25 15:00:19 -0400

    First Commit

---
+++ card.py
@@ -0,0 +1,7 @@
+class Card:
+
+    def __init__(self, value, suit):
+        self.value = ""
+        self.suit = ""
+
+    suit_list = ["Hearts", "Diamonds", "Spades", "Clubs"]
---
+++ deck_of_cards.py
@@ -0,0 +1,13 @@
+class Deck_of_Cards:
+
+    def __init__(self):
+        num_cards = 52
+        num_suits = 13
+        num_value = 4
+
+    def deal_cards(self):
+        # random number in range to determine dealt cards
+
+    def shuffle_cards(self):
+        # reset random number counter?
+
