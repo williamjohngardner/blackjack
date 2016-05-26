d4cca2f9c2c699db702d9946351f1afeebc62cfd
williamjohngardner <bill@williamjohngardner.com>
2016-05-26 17:25:47 -0400

    Thurs 5:25pm

--- blackjack.py
+++ blackjack.py
@@ -1,6 +1,6 @@
 # BLACKJACK GAME by Bill Gardner
 
-from hand import Hand
+from deck_of_cards import DeckOfCards
 
 print("*****************************" +
       "\n*****************************" +
@@ -8,39 +8,47 @@ print("*****************************" +
       "\n*****************************" +
       "\n*****************************")
 
-hand = Hand()
-
-num_cards_dealt = 2
+hand = DeckOfCards()
 max_cards_in_hand = 5
 player = []
 dealer = []
-h = ""
-s = ""
 
 
 # start = input("Press Enter To Start")
 
-player_card_1 = hand.deal(False)
-player_card_2 = hand.deal(False)
+player_card_1 = hand.deal_hand(False)
+player_card_2 = hand.deal_hand(False)
+dealer_card_1 = hand.deal_hand(False)
+dealer_card_2 = hand.deal_hand(False)
 
 player.append(player_card_1)
 player.append(player_card_2)
+dealer.append(dealer_card_1)
+dealer.append(dealer_card_2)
+
 print(player_card_1)
 print(player_card_2)
 print(player)
 
+print(dealer_card_1)
+print(dealer_card_2)
+print(dealer)
+
 while len(player) < max_cards_in_hand:
     choice = input("Would you like to Hit or Stand? H or S: ").lower()
-    if choice == h:
-        player_card = hand.deal(False)
+    if choice == "h":
+        player_card = hand.deal_hand(False)
         player.append(player_card)
         print(player)
         continue
     else:
-        # This will move to the dealer's turn
         break
 
-
-
-
-
+while len(dealer) < max_cards_in_hand:
+    if sum(dealer) <= 21:
+        dealer_card = hand.deal_hand(False)
+        dealer.append(dealer_card)
+        print(dealer)
+        continue
+    else:
+        break
--- deck_of_cards.py
+++ deck_of_cards.py
@@ -16,14 +16,15 @@ class DeckOfCards:
         self.shuffle = shuffle(self.deck)
         return self.shuffle
 
-    def deal_hand(self):
-        for card in self.deck:
-            self.dealt_card = choice(self.deck)
-        return self.dealt_card
-'''
+    def deal_hand(self, hide=False):
+        if hide == False:
+            for card in self.deck:
+                self.dealt_card = choice(self.deck)
+            return self.dealt_card
+        else:
+            return " _ "
+
 deck = DeckOfCards()
 deck.shuffle_deck()
 deck.deal_hand()
-# print(deck.dealt_card)
-# print(deck.dealt_card)
-'''
+print(deck.dealt_card)
