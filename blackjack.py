3957c752d327b9171d7e666fe3199c4dc8618aa4
williamjohngardner <bill@williamjohngardner.com>
2016-05-30 11:52:08 -0400

    Monday Noon

--- blackjack.py
+++ blackjack.py
@@ -1,134 +1,141 @@
 # BLACKJACK GAME by Bill Gardner
 
-# from deck_of_cards import DeckOfCards
-from hand import Player
+from deck_of_cards import DeckOfCards
 
 
 class BlackJack:
 
-    print("*****************************" +
-          "\n*****************************" +
-          "\n**** AWESOME BLACKJACK!!!****"
-          "\n*****************************" +
-          "\n*****************************")
-
-    # hand = DeckOfCards()
-    max_cards_in_hand = 5
-    player = []
-    dealer = []
-    face_cards = ["1", "J", "Q", "K"]
-    number_cards = "23456789"
-    ############# variables above will probably go away
-    players = Player()
-
-    # start = input("Press Enter To Start")
-    player1 = players.player_deal()
-    dealer1 = players.player_deal()
-    player_start_value = players.start_value
-    dealer_start_value = players.start_value
-
-    print("Player's Hand", player1)
-    print("Hand Value", players.start_value)
-    print("\nDealer's Hand", dealer1)
-    print("Hand Value", players.start_value)
-
-
-    '''
-    player_card_1 = hand.deal_hand(False)
-    player_card_2 = hand.deal_hand(False)
-    dealer_card_1 = hand.deal_hand(False)  # Don't forget to change this to True when the game is complete
-    dealer_card_2 = hand.deal_hand(False)
-    '''
-
-    '''
-    player_start_value = []
-
-    for card in player:
-        if card[0] in face_cards:
-            player_start_value.append(10)
-        elif card[0] in number_cards:
-            player_start_value.append(int(card[0]))
-        elif card[0] == "A":  # This logic is not yet bullet proof.  It needs to change based on the changing total.
-            if sum(player_start_value) <= 10:
-                player_start_value.append(11)
-            else:
-                player_start_value.append(1)
-        elif sum(player_start_value) == 21:
-            print("\nYOU HIT 21!!!  YOU WIN!!!")
-            exit()
-    '''
-
-    '''
-    while len(player) < max_cards_in_hand:
+    def __init__(self):
+        hand = DeckOfCards()
+        max_cards_in_hand = 5
+        player = []
+        dealer = []
+        face_cards = ["1", "J", "Q", "K"]
+        number_cards = "23456789"
         hand_value = 0
-        choice = input("Would you like to Hit or Stand? H or S: ").lower()
-        if choice == "h":
-            new_card = hand.deal_hand(False)
-            player.append(new_card)
-            for card in player:
-                if card[0] in face_cards:
-                    hand_value += 10
-                elif card[0] in number_cards:
-                    hand_value += int(card[0])
-                elif card[0] == "A":  # This logic is not yet bullet proof.  It needs to change based on the changing total.
-                        hand_value += 11
-                elif player_start_value == 21:
-                    print("\nYOU HIT 21!!!  YOU WIN!!!")
-                    exit()
-            print(player)
-            print(hand_value)
 
-            if hand_value < 21:
-                continue
-            elif hand_value == 21:
-                break
-            elif hand_value > 21:
-                print("\nGAME OVER! YOU'VE BUSTED!")
+        print("*****************************" +
+              "\n*****************************" +
+              "\n**** AWESOME BLACKJACK!!!****"
+              "\n*****************************" +
+              "\n*****************************")
+
+        # start = input("Press Enter To Start")
+
+        player_card_1 = hand.deal_hand(True)
+        player_card_2 = hand.deal_hand(True)
+        dealer_card_1 = hand.deal_hand(True)  # Don't forget to change this to False when the game is complete
+        dealer_card_2 = hand.deal_hand(True)
+
+        player.append(player_card_1)
+        player.append(player_card_2)
+        dealer.append(dealer_card_1)
+        dealer.append(dealer_card_2)
+
+        print("Player Hand: ", player)
+        print("Dealer Hand: ", dealer)
+        '''
+        player_start_value = []
+
+        for card in player:
+            if card[0] in face_cards:
+                player_start_value.append(10)
+            elif card[0] in number_cards:
+                player_start_value.append(int(card[0]))
+            elif card[0] == "A":  # This logic is not yet bullet proof.  It needs to change based on the changing total.
+                if sum(player_start_value) <= 10:
+                    player_start_value.append(11)
+                else:
+                    player_start_value.append(1)
+            elif sum(player_start_value) == 21:
+                print("\nYOU HIT 21!!!  YOU WIN!!!")
                 exit()
-        else:
-            break
-    '''
-    players.player_hand()
+        '''
+        while len(player) < max_cards_in_hand:
+            hand_value = 0
+            choice = input("Would you like to Hit or Stand? H or S: ").lower()
+            if choice == "h":
+                new_card = hand.deal_hand(True)
+                player.append(new_card)
+                for card in player:
+                    if card[0] in face_cards:
+                        hand_value += 10
+                    elif card[0] in number_cards:
+                        hand_value += int(card[0])
+                    elif card[0] == "A" and hand_value > 11:
+                        hand_value += 1
+                    elif card[0] == "A" and hand_value < 11:
+                        hand_value += 10
+                print(player)
+                print(hand_value)
 
-
-    dealer_start_value = []
-
-    for card in dealer:
-        if card[0] in face_cards:
-            dealer_start_value.append(10)
-        elif card[0] in number_cards:
-            dealer_start_value.append(int(card[0]))
-        elif card[0] == "A":  # This logic is not yet bullet proof.  It needs to change based on the changing total.
-            if sum(dealer_start_value) <= 10:
-                dealer_start_value.append(11)
+                if hand_value < 21:
+                    continue
+                elif hand_value == 21:
+                    break
+                elif hand_value > 21:
+                    break
             else:
-                dealer_start_value.append(1)
-        elif sum(dealer_start_value) == 21:
-            print("\nDEALER HIT 21!!!  DEALER WINS!!! GAME OVER!")
+                break
+
+        dealer_start_value = 0
+
+        for card in dealer:
+            if card[0] in face_cards:
+                dealer_start_value +=10
+            elif card[0] in number_cards:
+                dealer_start_value += int(card[0])
+            elif card[0] == "A":
+                if dealer_start_value <= 10:
+                    dealer_start_value += 11
+                else:
+                    dealer_start_value += 1
+            elif dealer_start_value == 21:
+                break
+
+        dealer_hand_value = dealer_start_value
+
+        while len(dealer) < max_cards_in_hand:
+            if 21 >= dealer_hand_value >= 17:
+                break
+            elif dealer_hand_value < 17:
+                new_card = hand.deal_hand(True)
+                dealer.append(new_card)
+                for card in dealer:
+                    if card[0] in face_cards:
+                        dealer_hand_value += 10
+                    elif card[0] in number_cards:
+                        dealer_hand_value += int(card[0])
+                    elif card[0] == "A" and dealer_hand_value > 11:
+                        dealer_hand_value += 1
+                    elif card[0] == "A" and dealer_hand_value < 11:
+                        dealer_hand_value += 10
+                print(dealer, dealer_hand_value)
+            elif dealer_hand_value > 21:
+                break
+            else:
+                break
+
+        if dealer_hand_value > 21:
+            print("You Win! Dealer Busted!")
+            exit()
+        elif hand_value > 21:
+            print("You've Busted! Game Over!")
+        elif player == 21:
+            if player == dealer:
+                print("Tie! Dealer Wins!")
+                exit()
+            else:
+                print("BlackJack!  You Win!")
+                exit()
+        elif player > dealer:
+            print("Player wins!")
+            exit()
+        elif dealer < player:
+            print("Dealer wins!")
+            exit()
+        elif player == dealer:
+            print("Tie! Dealer wins!")
             exit()
 
-
-    while len(dealer) < max_cards_in_hand:
-        dealer_hand_value = 0
-        if dealer_hand_value <= 17:
-            new_card = hand.deal_hand(False)
-            dealer.append(new_card)
-            for card in dealer:
-                if card[0] in face_cards:
-                    dealer_hand_value += 10
-                elif card[0] in number_cards:
-                    dealer_hand_value += int(card[0])
-                elif card[0] == "A":  # This logic is not yet bullet proof.  It needs to change based on the changing total.
-                    dealer_hand_value += 11
-                else:
-                    dealer_hand_value.append('#')
-
-            print(dealer, dealer_hand_value)
-
-            if dealer_hand_value <= 17:
-                continue
-            elif dealer_hand_value == 21:
-                break
-            elif dealer_hand_value > 21:
-                print("\nDEALER BUSTED!!!  YOU WIN!!!")
-                exit()
+blackjack = BlackJack()
--- hand.py
+++ hand.py
@@ -13,28 +13,6 @@ class Player:
         self.hand_value = 0
         self.start_value = 0
 
-
-    def player_deal(self):
-
-        player_card_1 = self.hand.deal_hand(True)
-        player_card_2 = self.hand.deal_hand(True)
-
-        self.player.append(player_card_1)
-        self.player.append(player_card_2)
-
-        return self.player
-
-    '''
-    def dealer_deal(self):
-        dealer_card_1 = self.hand.deal_hand(True)  # Don't forget to change this to True when the game is complete
-        dealer_card_2 = self.hand.deal_hand(False)
-
-        self.dealer.append(dealer_card_1)
-        self.dealer.append(dealer_card_2)
-
-        return self.dealer
-    '''
-
     def start_value(self):
 
         for card in self.player:
@@ -51,31 +29,4 @@ class Player:
                 break
         return self.start_value
 
-    def player_hand(self):
 
-        while len(self.player) < self.max_cards_in_hand:
-            choice = input("Would you like to Hit or Stand? H or S: ").lower()
-            if choice == "h":
-                new_card = self.hand.deal_hand(False)
-                self.player.append(new_card)
-                for card in self.player:
-                    if card[0] in self.face_cards:
-                        self.hand_value += 10
-                    elif card[0] in self.number_cards:
-                        self.hand_value += int(card[0])
-                    elif card[0] == "A":  # This logic is not bullet proof.
-                        self.hand_value += 11
-                    elif self.player_start_value == 21:
-                        print("\nYOU HIT 21!!!  YOU WIN!!!")
-                        exit()
-
-                if self.hand_value < 21:
-                    continue
-                elif self.hand_value == 21:
-                    break
-                elif self.hand_value > 21:
-                    print("\nGAME OVER! YOU'VE BUSTED!")
-                    exit()
-            else:
-                break
-        return self.hand_value
